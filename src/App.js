38123245006e0da14aa516778889b3559b457cbb
Sone <sthaya@gmail.com>
2020-09-23 21:30:02 -0700

    added logo image, installed sass loader, styled header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-
-
+import React from "react";
+import Header from "./components/Header";
+import "./default.scss";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h2>Hello World!</h2>
     </div>
   );
