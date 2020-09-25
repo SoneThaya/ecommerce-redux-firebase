75db4854a8819e93f3555f99bcc505e2e5a4d690
Sone <sthaya@gmail.com>
2020-09-24 21:09:24 -0700

    created footer component and styles

--- src/layouts/HomepageLayout.js
+++ src/layouts/HomepageLayout.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const HomepageLayout = (props) => {
   return (
     <div className="fullHeight">
       <Header />
-      <div className="main">{props.children}</div>
+      {props.children}
+      <Footer />
     </div>
   );
 };
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const MainLayout = (props) => {
@@ -6,6 +7,7 @@ const MainLayout = (props) => {
     <div>
       <Header />
       <div className="main">{props.children}</div>
+      <Footer />
     </div>
   );
 };
