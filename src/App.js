1f1fb19e5aac203cf27e126c33cb5ffac8e63926
Sone <sthaya@gmail.com>
2020-09-24 20:46:15 -0700

    installed react router dom and created registeration component and styles scss files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import "./default.scss";
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <Homepage />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/registration" component={Registration} />
+        </Switch>
       </div>
     </div>
   );
