583566fa03e15cc76ab319e796ddff670c7b2e88
Sone <sthaya@gmail.com>
2020-09-24 18:00:06 -0700

    created directory components and styles component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import "./default.scss";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h2>Hello World!</h2>
+      <Homepage />
     </div>
   );
 }
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/Directory";
+
+
+const Homepage = (props) => {
+  return (
+    <section>
+      <Directory />
+    </section>
+  );
+};
+
+export default Homepage;
