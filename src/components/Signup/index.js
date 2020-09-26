9195558b17bb22a9f6187d92118b121133c872e8
Sone <sthaya@gmail.com>
2020-09-26 15:10:21 -0700

    set up sign up form and styled

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,23 +1,82 @@
-import React, { Component } from 'react'
-import './styles.scss'
+import React, { Component } from "react";
+import "./styles.scss";
+
+import FormInput from "../forms/FormInput";
+import Button from "../forms/Button";
+
+const initialState = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+
+    this.handleChange = this.handleChange.bind();
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+
     return (
       <div className="signup">
         <div className="wrap">
-          <h2>
-            Signup
-          </h2>
+          <h2>Signup</h2>
+          <div className="formWrap">
+            <form>
+              <FormInput
+                type="text"
+                name="displayName"
+                value={displayName}
+                placeholder="Full name"
+                onChange={this.handleChange}
+              />
 
-          <form>
-          
-          </form>
+              <FormInput
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                onChange={this.handleChange}
+              />
+
+              <FormInput
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                onChange={this.handleChange}
+              />
+
+              <FormInput
+                type="password"
+                name="confirmPassword"
+                value={confirmPassword}
+                placeholder="Confirm password"
+                onChange={this.handleChange}
+              />
+
+              <Button type="submit">Register</Button>
+            </form>
+          </div>
         </div>
-        
       </div>
-    )
+    );
   }
 }
 
-export default Signup
+export default Signup;
