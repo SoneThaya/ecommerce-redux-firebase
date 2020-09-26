1290ce3b6c830107a80e92486faa1448e770dc06
Sone <sthaya@gmail.com>
2020-09-26 14:49:41 -0700

    created sign up component and styles file

---
+++ src/components/Signup/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './styles.scss'
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="signup">
+        <div className="wrap">
+          <h2>
+            Signup
+          </h2>
+
+          <form>
+          
+          </form>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default Signup
--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
+import Signup from '../../components/Signup';
 
 import './styles.scss'
 
 class Registration extends Component {
   render() {
-    return (
-      <div>
-        <h1>Registration</h1>
-      </div>
-    );
+    return <Signup />
   }
 }
 
