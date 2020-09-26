88d3c53695266bfa5ad7eed72958851efe0862dc
Sone <sthaya@gmail.com>
2020-09-25 17:51:28 -0700

    installed firebase, created files for firebase

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,6 +19,9 @@ const Header = (props) => {
             <li>
               <Link to="/registration">Register</Link>
             </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
           </ul>
         </div>
       </div>
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtItqtncHGbLcqFbp9tZ3CJZ1NwXid1og",
+  authDomain: "ecommerce-site-c9bd3.firebaseapp.com",
+  databaseURL: "https://ecommerce-site-c9bd3.firebaseio.com",
+  projectId: "ecommerce-site-c9bd3",
+  storageBucket: "ecommerce-site-c9bd3.appspot.com",
+  messagingSenderId: "942555625503",
+  appId: "1:942555625503:web:9181f2fb0cbf14de5b3d2a",
+};
