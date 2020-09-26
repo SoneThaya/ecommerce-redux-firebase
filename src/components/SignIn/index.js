fb4e6665be5fbf798edc129b3826eacfd60f5a19
Sone <sthaya@gmail.com>
2020-09-25 18:16:48 -0700

    set up google sign in with a pop up

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import './styles.scss'
+import React, { Component } from "react";
+import Button from "../forms/Button";
+import "./styles.scss";
+import { signInWithGoogle } from "../../firebase/utils";
 
-const SignIn = (props) => {
-  return (
-    <div className="signin">
-      <div className="wrap">
-        <h2>Login</h2>
+class SignIn extends Component {
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    
+  }
+
+  render() {
+    return (
+      <div className="signin">
+        <div className="wrap">
+          <h2>Login</h2>
+
+          <div className="formWrap">
+            <form onSubmit={this.handleSubmit}>
+              <div className="socialSignin">
+                <div className="row">
+                  <Button onClick={signInWithGoogle}>
+                    Sign in with Google
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default SignIn
+export default SignIn;
