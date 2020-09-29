58d267a9a9a88f16c8ad3f67e6d2c1c94737e44b
Sone <sthaya@gmail.com>
2020-09-28 21:14:26 -0700

    refactored sign up component to make it a functional component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { useSelector } from "react-redux";
 import "./styles.scss";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/utils";
 
 import Logo from "../../assets/logo1.png";
 
+const mapState = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const Header = (props) => {
-  const { currentUser } = props;
+  const { currentUser } = useSelector(mapState);
 
   return (
     <header className="header">
@@ -47,9 +51,4 @@ Header.defaultProps = {
   currentUser: null,
 };
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
-})
-
-
-export default connect(mapStateToProps, null)(Header);
+export default Header;
